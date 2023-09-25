c59b9ede2ff01d5016b46353e5b9b76bbedc0c0f
muratapk <muratapk@hotmail.com>
2023-09-25 18:55:39 +0300

    Add project files.

---
+++ eticaretuygulamasi/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using eticaretuygulamasi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eticaretuygulamasi.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Procducts { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ eticaretuygulamasi/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eticaretuygulamasi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string  ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductNumber { get; set; }
+        public float ProductPrice { get; set; }
+        public string  ProductPicture { get; set; }
+        public  int  CategoryId  { get; set; }
+        public virtual Category Category { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Resim Seçiniz")]
+
+        public IFormFile resimyukle { get; set; }
+    }
+}
